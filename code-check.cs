4ec61acfa802b21221d854a4b0ef4b0533f0f590
iamsunnyyz <sunny170600@gmail.com>
2024-08-24 08:50:22 +0530

    new file added

--- code-check.cs
+++ code-check.cs
@@ -1,48 +1,105 @@
-// Optimized C# implementation of Bubble sort
+// C# program for Merge Sort
 using System;
 
-class GFG {
-    // An optimized version of Bubble Sort
-    static void bubbleSort(int[] arr, int n)
-    {
-        int i, j, temp;
-        bool swapped;
-        for (i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    
-                    // Swap arr[j] and arr[j+1]
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
+class GfG {
 
-            // If no two elements were
-            // swapped by inner loop, then break
-            if (swapped == false)
-                break;
+    // Merges two subarrays of []arr.
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    static void merge(int[] arr, int l, int m, int r)
+    {
+        // Find sizes of two
+        // subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        int i, j;
+
+        // Copy data to temp arrays
+        for (i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        // Merge the temp arrays
+
+        // Initial indexes of first
+        // and second subarrays
+        i = 0;
+        j = 0;
+
+        // Initial index of merged
+        // subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements
+        // of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements
+        // of R[] if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
         }
     }
 
-    // Function to print an array
-    static void printArray(int[] arr, int size)
+    // Main function that
+    // sorts arr[l..r] using
+    // merge()
+    static void mergeSort(int[] arr, int l, int r)
     {
-        int i;
-        for (i = 0; i < size; i++)
+        if (l < r) {
+
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    // A utility function to
+    // print array of size n
+    static void printArray(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
             Console.Write(arr[i] + " ");
         Console.WriteLine();
     }
 
-    // Driver method
-    public static void Main()
+    // Driver code
+    public static void Main(String[] args)
     {
-        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
-        int n = arr.Length;
-        bubbleSort(arr, n);
-        Console.WriteLine("Sorted array:");
-        printArray(arr, n);
+        int[] arr = { 12, 11, 13, 5, 6, 7 };
+        Console.WriteLine("Given array is");
+        printArray(arr);
+        mergeSort(arr, 0, arr.Length - 1);
+        Console.WriteLine("\nSorted array is");
+        printArray(arr);
     }
 }
